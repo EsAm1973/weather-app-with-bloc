3e8cc7539c04c0e703d5a016cb7a0c6f70f0b17e
EsAm1973 <me354172@gmail.com>
2024-10-31 15:45:28 +0300

    create bloc enviroment

--- lib/buisness_logic_layer/cubit/weather_cubit.dart
+++ lib/buisness_logic_layer/cubit/weather_cubit.dart
@@ -1,8 +1,28 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:weather_app_with_bloc/date/model/current_weather.dart';
+import 'package:weather_app_with_bloc/date/model/forecast_weather.dart';
+import 'package:weather_app_with_bloc/date/repository/weather_repository.dart';
 
 part 'weather_state.dart';
 
 class WeatherCubit extends Cubit<WeatherState> {
-  WeatherCubit() : super(WeatherInitial());
+  final WeatherRepository weatherRepository;
+  CurrentWeather? currentWeather;
+  List<ForecastWeather>? forecastWeather;
+  WeatherCubit(this.weatherRepository) : super(WeatherInitial());
+
+  CurrentWeather? fetchCurrentWeather(String city) {
+    weatherRepository.getWeatherData(city).then((weather) {
+      emit(CurrentWeatherLoaded(currentWeather: currentWeather));
+    });
+    return currentWeather;
+  }
+
+  List<ForecastWeather>? fetchForecastWeather(String city) {
+    weatherRepository.getWeatherForecast(city).then((forecast) {
+      emit(ForecasrWeatherLoaded(forecastWeather: forecastWeather));
+    });
+    return forecastWeather;
+  }
 }
--- lib/buisness_logic_layer/cubit/weather_state.dart
+++ lib/buisness_logic_layer/cubit/weather_state.dart
@@ -4,3 +4,15 @@ part of 'weather_cubit.dart';
 sealed class WeatherState {}
 
 final class WeatherInitial extends WeatherState {}
+
+class CurrentWeatherLoaded extends WeatherState {
+  final CurrentWeather? currentWeather;
+
+  CurrentWeatherLoaded({required this.currentWeather});
+}
+
+class ForecasrWeatherLoaded extends WeatherState {
+  final List<ForecastWeather>? forecastWeather;
+
+  ForecasrWeatherLoaded({required this.forecastWeather});
+}
