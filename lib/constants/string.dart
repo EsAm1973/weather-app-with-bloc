958e264ca6d59278fd152f8f96007a45f314c23d
EsAm1973 <me354172@gmail.com>
2024-10-31 14:42:48 +0300

    First Upload

---
+++ lib/constants/string.dart
@@ -0,0 +1,9 @@
+const String baseUrl = 'https://api.openweathermap.org/data/2.5';
+const String apiKey = 'dce1390fc9d5c4aeaeb8a4168b9e580d';
+
+// Weather
+// final finalUrl = '$baseUrl/weather?q=$city&appid=$apiKey';
+
+
+// Weather Forecast
+// final url = Uri.parse('$baseUrl/forecast?q=$cityName&appid=$apiKey');
